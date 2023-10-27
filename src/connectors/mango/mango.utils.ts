e002421131f734b8be3998b4d3e708d5fc2ff6ce
mlguys <machinelearningguys@gmail.com>
2023-10-27 16:01:56 +0700

    add order status tracker

--- src/connectors/mango/mango.utils.ts
+++ src/connectors/mango/mango.utils.ts
@@ -30,3 +30,119 @@ export function translateOrderType(type: OrderType) {
 export function randomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export enum OrderStatus {
+  CREATED = 'CREATED',
+  OPEN = 'OPEN',
+  FILLED = 'FILLED',
+  PARTIALLY_FILLED = 'PARTIALLY_FILLED',
+  CANCELLED = 'CANCELLED',
+  EXPIRED = 'EXPIRED',
+}
+
+export type OrderTrackingInfo = {
+  clientOrderId: number;
+  exchangeOrderId?: string;
+  status: OrderStatus;
+  orderAmount: string;
+  filledAmount?: string;
+};
+
+export class OrderTracker {
+  private clientOrderIdToTrackingInfo: Map<number, OrderTrackingInfo> =
+    new Map();
+
+  public addOrder(clientOrderId: number, orderAmount: string) {
+    this.clientOrderIdToTrackingInfo.set(clientOrderId, {
+      clientOrderId,
+      status: OrderStatus.CREATED,
+      orderAmount,
+    });
+  }
+
+  public updateOrderExchangeOrderId(
+    clientOrderId: number,
+    exchangeOrderId: string
+  ) {
+    const trackingInfo = this.clientOrderIdToTrackingInfo.get(clientOrderId);
+    if (trackingInfo) {
+      trackingInfo.exchangeOrderId = exchangeOrderId;
+    }
+  }
+
+  public updateOrderStatus(
+    clientOrderId: number,
+    status: OrderStatus,
+    filledAmount?: string
+  ) {
+    const trackingInfo = this.clientOrderIdToTrackingInfo.get(clientOrderId);
+    if (trackingInfo) {
+      trackingInfo.status = status;
+
+      if (filledAmount) {
+        trackingInfo.filledAmount = filledAmount;
+      }
+    }
+  }
+
+  public updateOrderStatusByExchangeOrderId(
+    exchangeOrderId: string,
+    status: OrderStatus,
+    filledAmount?: string
+  ) {
+    const trackingInfo = Array.from(
+      this.clientOrderIdToTrackingInfo.values()
+    ).find((info) => info.exchangeOrderId === exchangeOrderId);
+    if (trackingInfo) {
+      trackingInfo.status = status;
+
+      if (filledAmount) {
+        trackingInfo.filledAmount = filledAmount;
+      }
+    }
+  }
+
+  public getExchangeOrderId(clientOrderId: number) {
+    const trackingInfo = this.clientOrderIdToTrackingInfo.get(clientOrderId);
+    if (trackingInfo) {
+      return trackingInfo.exchangeOrderId;
+    }
+
+    return undefined;
+  }
+
+  public getClientOrderId(exchangeOrderId: string) {
+    const trackingInfo = Array.from(
+      this.clientOrderIdToTrackingInfo.values()
+    ).find((info) => info.exchangeOrderId === exchangeOrderId);
+    if (trackingInfo) {
+      return trackingInfo.clientOrderId;
+    }
+
+    return undefined;
+  }
+
+  public getOrderTrackingInfo(clientOrderId: number) {
+    const trackingInfo = this.clientOrderIdToTrackingInfo.get(clientOrderId);
+    if (trackingInfo) {
+      return trackingInfo;
+    }
+
+    return undefined;
+  }
+
+  public getOrderTrackingInfoByExchangeOrderId(exchangeOrderId: string) {
+    const trackingInfo = Array.from(
+      this.clientOrderIdToTrackingInfo.values()
+    ).find((info) => info.exchangeOrderId === exchangeOrderId);
+    if (trackingInfo) {
+      return trackingInfo;
+    }
+
+    return undefined;
+  }
+
+  public getAllOrderTrackingInfo() {
+    return Array.from(this.clientOrderIdToTrackingInfo.values());
+  }
+}
